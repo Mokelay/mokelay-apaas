4689fea98217f4c8a15895d9b1bf65c9a9231e77
CarlChen <carl@greatbee.com>
2023-08-23 21:30:02 +0800

    add component

---
+++ src/component/m-text/m-text.desc.js
@@ -0,0 +1,20 @@
+var desc = {
+	name:"文本",
+	icon:"", //如何配置？
+	tagName: "m-text",
+	attributesDesc:[
+		{
+		}
+	],
+	methodsDesc:[
+		{
+		}
+	],
+	eventsDesc:[
+		{}
+	],
+	attributesEditorView:{},
+	supportAttributeConfig:true,
+	supportActionConfig:true,
+	supportStyleConfig:true
+}
