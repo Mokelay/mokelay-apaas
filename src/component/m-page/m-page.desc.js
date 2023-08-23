216c0262c9da93d03132e47c52609110f54de832
CarlChen <carl@greatbee.com>
2023-08-23 21:15:43 +0800

    update

--- src/component/m-page/m-page.desc.js
+++ src/component/m-page/m-page.desc.js
@@ -1,21 +1,47 @@
-{
-	name:"页面组件",
+var desc = {
+	name:"页面",
 	icon:"", //如何配置？
 	tagName: "m-page",
 	attributesDesc:[
 		{
-			name: "View配置列表",
-			varName,"views",
-			dataType:"Array"
+			varShowName: "View配置列表",
+			varCodeName,"views",
+			varDataType:"Array",
+			varDataDesc:null, //此处描述View的数据结构，因为不是用于配置的，所以就不用写DSL了
+			defaultValue:[]
 		}
 	],
 	methodsDesc:[
-		{}
+		{
+			funcShowName:"刷新页面",
+			funcCodeName:"refresh",
+			params:[],
+			returnDataType:null
+		},
+		{
+			funcShowName:"调整页面大小",
+			funcCodeName:"resize",
+			params:[
+				{
+					varShowName: "宽度",
+					varCodeName,"width",
+					varDataType:"Integer",
+					defaultValue:1024
+				},
+				{
+					varShowName: "高度",
+					varCodeName,"height",
+					varDataType:"Integer",
+					defaultValue:768
+				}
+			],
+			returnDataType:null
+		}
 	],
 	eventsDesc:[
 		{}
 	],
-	attributeConfig:false,
-	eventConfig:true,
-	styleConfig:true
+	supportAttributeConfig:false,
+	supportActionConfig:true,
+	supportStyleConfig:true
 }
