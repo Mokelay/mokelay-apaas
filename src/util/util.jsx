7948deb2a137fc31cf9195092955990eeacb6075
CarlChen <carl@greatbee.com>
2023-08-30 16:35:06 +0800

    Update util

--- src/util/util.jsx
+++ src/util/util.jsx
@@ -1,3 +1,49 @@
+import axios from 'axios';
+import _ from 'underscore';
+import Qs from 'qs';
+
 export default {
-	
+	invoke : function(options) {
+		return new Promise((resolve, reject) => {
+		    if (!util.ajax) {
+		        util.ajax = axios.create({
+		            baseURL: window._TY_APIHost,
+		            timeout: 30000,
+		            withCredentials: true
+		        });
+		    }
+		    util.ajax(options).then(function(response) {
+		        if (response && response['data'] && response['data']['code'] && response['data']['code'] == -420 && util.isWX()) {
+		            //微信端没有登录，跳转微信授权
+		            location.href = response['data']['message'] || window._TY_SSOURL;
+		        } else if (response && response['data'] && response['data']['code'] && response['data']['code'] <= -400) {
+		            //所有Code小于等于-400都是属于没有登录授权的，统一走SSOURL配置路径
+		            if (response['data']['code'] == -401) {
+		                location.href = window._RS_SSOURL;
+		            } else {
+		                location.href = window._TY_SSOURL;
+		            }
+		        } else {
+		            resolve(response);
+		        }
+		        // if (response && response['data'] && response['data']['code'] && response['data']['code'] == -420 && util.isWX()) {
+		        //     //微信端没有登录，跳转微信授权
+		        //     location.href = response['data']['message'] || window._TY_SSOURL;
+		        // } else if (response && response['data'] && response['data']['code'] && response['data']['code'] == -401) {
+		        //     //龙眼专用 未登录
+		        //     location.href = window._TY_SSOURL;
+		        // } else if (response && response['data'] && response['data']['code'] && response['data']['code'] == -400) {
+		        //     //TY E端未登录
+		        //     location.href = document.location.protocol + "//" + document.location.host + "/#/ty-login";
+		        // } else if (response && response['data'] && response['data']['code'] && response['data']['code'] == -410) {
+		        //     //TY B端未登录
+		        //     location.href = document.location.protocol + "//" + document.location.host + "/#/ty_b_login";
+		        // } else {
+		        //     resolve(response);
+		        // }
+		    }).catch(function(error) {
+		        reject(error);
+		    });
+		});
+	}
 };
