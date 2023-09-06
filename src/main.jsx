0661065e0b34071ee1ad5ee84dfef69dac25029c
CarlChen <carl@greatbee.com>
2023-09-06 15:13:14 +0800

    优化代码结构

--- src/main.jsx
+++ src/main.jsx
@@ -13,12 +13,15 @@ import {
 // 全局样式
 import './styles/index.less';
 
-// 加载MUI基础组件库
+// 加载基础组件库
 const componentMap = {};
 const baseComponents = import.meta.globEager(['./component/*/index.jsx', './editor/*/index.jsx']);
 Object.values(baseComponents).forEach(function (baseComp) {
   componentMap[baseComp.default.name] = baseComp.default;
 });
+
+//加载工具包
+import Util from './util/util.jsx';
 
 // 加载自定义组件库，加载完成后，后续还可以还可以动态加载
 
@@ -28,24 +31,19 @@ window.__Mokelay = {
 };
 
 //加载内置巴斯
-import internalBuzzs from './util/internal_buzzs.jsx';
-import Util from './util/util.jsx';
+import InternalBuzzs from './util/internal_buzzs.jsx';
 
 // 加载内置变量
-window.__Mokelay.InternalVar = internalBuzzs['internalVar'];
-window.__Mokelay.InternalVarDesc = internalBuzzs['internalVarDesc'];
+window.__Mokelay.InternalVar = InternalBuzzs['internalVar'];
+window.__Mokelay.InternalVarDesc = InternalBuzzs['internalVarDesc'];
 // 加载内置函数
-window.__Mokelay.InternalFunc = internalBuzzs['internalFunc'];
-window.__Mokelay.InternalFuncDesc = internalBuzzs['internalFuncDesc'];
+window.__Mokelay.InternalFunc = InternalBuzzs['internalFunc'];
+window.__Mokelay.InternalFuncDesc = InternalBuzzs['internalFuncDesc'];
 
 // 通过URL获取页面参数，获取页面DSL， 目前可以先从dsl/目录里本地加载，方便测试
 
 //App信息
 import appList from '../dsl/app_list.js';
-
-//依赖的数据源
-import data from '../dsl/data.js';
-
 //导入UI配置信息，开发环境为JS Config， 生产环境为接口获取
 const appMap = {};
 appList.map(function (_app) {
