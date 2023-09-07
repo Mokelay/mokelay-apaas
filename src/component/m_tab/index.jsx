86c0b340e0a4fecb247dcf127b4e3d4e9b22f080
CarlChen <carl@greatbee.com>
2023-09-07 23:43:52 +0800

    改在M_Tab

--- src/component/m_tab/index.jsx
+++ src/component/m_tab/index.jsx
@@ -1,7 +1,17 @@
 /**
  * M_Tab
- * 
+ *
  * */
- export default function M_Tab(){
+// eslint-disable-next-line no-unused-vars
+import { forwardRef, useRef, useImperativeHandle } from 'react';
 
-}
+const M_Tab = forwardRef(function M_Tab(props, ref) {
+  useImperativeHandle(
+    ref,
+    () => {
+      return {};
+    },
+    [],
+  );
+});
+export default M_Tab;
