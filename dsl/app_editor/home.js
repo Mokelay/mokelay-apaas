e6428ebccb470b62bc5d098cfe2f80076bc15364
CarlChen <carl@greatbee.com>
2023-09-07 21:13:29 +0800

    处理editor 与m-page

--- dsl/app_editor/home.js
+++ dsl/app_editor/home.js
@@ -7,18 +7,30 @@ export default {
     name: 'page',
     component: 'M_Page',
     children: [
-	{
-		uuid:"view_1_1",
-		name:"文本1",
-		component:"M_Text",
-		category:"Single",
-		attributes:[
-			{
-				varCodeName:"content",
-				value:"这是Mokelay编辑器应用"
-			}
-		]
-	}
-    ]
-  }
+      {
+        uuid: 'view_1_1',
+        name: '文本1',
+        component: 'M_Text',
+        category: 'Single',
+        attributes: [
+          {
+            varCodeName: 'content',
+            value: '这是Mokelay编辑器应用',
+          },
+        ],
+      },
+      {
+        uuid: 'view_1_2',
+        name: '文本2',
+        component: 'M_Text',
+        category: 'Single',
+        attributes: [
+          {
+            varCodeName: 'content',
+            value: '用于测试编辑器的',
+          },
+        ],
+      },
+    ],
+  },
 };
--- src/component/m_page/index.jsx
+++ src/component/m_page/index.jsx
@@ -3,17 +3,56 @@
  *
  * */
 
-import React from 'react';
+// import React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 
+// eslint-disable-next-line react/prop-types
 export default function M_Page({ children }) {
   children = children || [];
   //TODO xs={4} 的设置是在容器里，还是在组件里？
-  const ChildrenViews = children.map( view => <Grid key={view.key} item xs={8}>{view}</Grid>);
+  // eslint-disable-next-line react/prop-types
+  const ChildrenViews = children.map((view) => (
+    <Grid key={view.key} item xs={8}>
+      {view}
+    </Grid>
+  ));
+
+  const mouseDown = function () {
+    console.log('mouse down..');
+  };
+  const mouseEnter = function () {
+    console.log('mouse enter..');
+  };
+  const mouseLeave = function () {
+    console.log('mouse leave..');
+  };
+  const mouseMove = function (e) {
+    console.log(e);
+    console.log('mouse move..');
+  };
+  const mouseOut = function () {
+    console.log('mouse out..');
+  };
+  const mouseOver = function () {
+    console.log('mouse over..');
+  };
+  const mouseUp = function () {
+    console.log('mouse up..');
+  };
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
+      <Grid
+        container
+        spacing={2}
+        onMouseDown={mouseDown}
+        onMouseEnter={mouseEnter}
+        onMouseLeave={mouseLeave}
+        onMouseMove={mouseMove}
+        onMouseOut={mouseOut}
+        onMouseOver={mouseOver}
+        onMouseUp={mouseUp}
+      >
         {ChildrenViews}
       </Grid>
     </Box>
