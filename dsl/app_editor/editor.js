0d859caec19b5dec50ad9287ea7aebdef5dfce6f
CarlChen <carl@greatbee.com>
2023-09-07 15:06:05 +0800

    copy apaas dom

---
+++ dsl/app_editor/editor.js
@@ -0,0 +1,12 @@
+export default {
+  uuid: 'editor',
+  appUUID: 'app_editor',
+  title: 'Home',
+  view: {
+    uuid: 'view_1_1',
+    name: '编辑器',
+    component: 'M_Layout_Edit',
+    category: 'Single',
+    attributes: [],
+  },
+};
