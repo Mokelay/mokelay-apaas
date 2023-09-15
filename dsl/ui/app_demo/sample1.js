03fd7b62cf1fbbbdff73f54ae02d08aaa26fcad6
CarlChen <carl@greatbee.com>
2023-09-15 20:34:37 +0800

    sample内容增加scroll

--- dsl/ui/app_demo/sample1.js
+++ dsl/ui/app_demo/sample1.js
@@ -36,6 +36,173 @@ export default {
         events: [],
       },
       {
+        uuid: 'view_1_2_a',
+        name: '文本1',
+        component: 'M_Text',
+        category: 'Single',
+        attributes: [
+          {
+            varCodeName: 'initContent',
+            value: '文本内容2',
+          },
+        ],
+        style: [],
+        events: [],
+      },
+      {
+        uuid: 'view_1_3',
+        name: '文本1',
+        component: 'M_Text',
+        category: 'Single',
+        attributes: [
+          {
+            varCodeName: 'initContent',
+            value: '文本内容3',
+          },
+        ],
+        style: [],
+        events: [],
+      },
+      {
+        uuid: 'view_1_4',
+        name: '文本1',
+        component: 'M_Text',
+        category: 'Single',
+        attributes: [
+          {
+            varCodeName: 'initContent',
+            value: '文本内容4',
+          },
+        ],
+        style: [],
+        events: [],
+      },
+
+      {
+        uuid: 'view_1_5',
+        name: '文本1',
+        component: 'M_Text',
+        category: 'Single',
+        attributes: [
+          {
+            varCodeName: 'initContent',
+            value: '文本内容1eee',
+            i18n: true,
+            i18nConfig: {
+              'zh-cn': '文本内容1',
+              'en-us': 'Text Content 1',
+            },
+          },
+        ],
+        style: [],
+        events: [],
+      },
+      {
+        uuid: 'view_1_6_a',
+        name: '文本1',
+        component: 'M_Text',
+        category: 'Single',
+        attributes: [
+          {
+            varCodeName: 'initContent',
+            value: '文本内容2gggg',
+          },
+        ],
+        style: [],
+        events: [],
+      },
+      {
+        uuid: 'view_7_3',
+        name: '文本1',
+        component: 'M_Text',
+        category: 'Single',
+        attributes: [
+          {
+            varCodeName: 'initContent',
+            value: '文本内容3ffff',
+          },
+        ],
+        style: [],
+        events: [],
+      },
+      {
+        uuid: 'view_1_8',
+        name: '文本1',
+        component: 'M_Text',
+        category: 'Single',
+        attributes: [
+          {
+            varCodeName: 'initContent',
+            value: '文本内容4eee',
+          },
+        ],
+        style: [],
+        events: [],
+      },
+
+      {
+        uuid: 'view_1_10',
+        name: '文本1',
+        component: 'M_Text',
+        category: 'Single',
+        attributes: [
+          {
+            varCodeName: 'initContent',
+            value: '文本内容1dddd',
+            i18n: true,
+            i18nConfig: {
+              'zh-cn': '文本内容1',
+              'en-us': 'Text Content 1',
+            },
+          },
+        ],
+        style: [],
+        events: [],
+      },
+      {
+        uuid: 'view_1_2_a1',
+        name: '文本1',
+        component: 'M_Text',
+        category: 'Single',
+        attributes: [
+          {
+            varCodeName: 'initContent',
+            value: '文本内容2ccc',
+          },
+        ],
+        style: [],
+        events: [],
+      },
+      {
+        uuid: 'view_1_32',
+        name: '文本1',
+        component: 'M_Text',
+        category: 'Single',
+        attributes: [
+          {
+            varCodeName: 'initContent',
+            value: '文本内容3bbb',
+          },
+        ],
+        style: [],
+        events: [],
+      },
+      {
+        uuid: 'view_1_34',
+        name: '文本1',
+        component: 'M_Text',
+        category: 'Single',
+        attributes: [
+          {
+            varCodeName: 'initContent',
+            value: '文本内容4aaa',
+          },
+        ],
+        style: [],
+        events: [],
+      },
+
+      {
         uuid: 'view_1_2',
         name: '数据表格1',
         component: 'M_Table',
