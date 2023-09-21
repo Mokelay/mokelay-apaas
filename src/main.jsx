d0d7e2f56add3614ec115b1a326e5e3cec9bb8a3
CarlChen <carl@greatbee.com>
2023-09-21 13:31:20 +0800

    把UI render独立组件化

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,6 @@
-import Util from './util/util.jsx';
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
-import {
-  HashRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-  useParams,
-  useLocation,
-} from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 // 全局样式
 import './styles/index.less';
@@ -43,83 +35,7 @@ window.__Mokelay.InternalVarDesc = InternalBuzzs['internalVarDesc'];
 window.__Mokelay.InternalFunc = InternalBuzzs['internalFunc'];
 window.__Mokelay.InternalFuncDesc = InternalBuzzs['internalFuncDesc'];
 
-/**
- * 通过URL获取页面参数，获取页面DSL，目前可以先从dsl/ui/目录里本地加载，方便测试
- */
-//App信息
-import appList from '../dsl/ui/app_list.js';
-//导入UI配置信息，开发环境为JS Config， 生产环境为接口获取
-const appMap = {};
-appList.map(function (_app) {
-  appMap[_app['uuid']] = {
-    app: _app,
-    uiMap: {},
-  };
-});
-const fs = import.meta.globEager('../dsl/ui/*/*.js');
-Object.keys(fs).forEach(function (f) {
-  var _tmp = f.split('/');
-  if (_tmp.length == 5) {
-    const appUUID = _tmp[3];
-    const uiUUID = f.substr(f.lastIndexOf('/') + 1, f.lastIndexOf('.') - f.lastIndexOf('/') - 1);
-    if (appMap[appUUID]) {
-      appMap[appUUID]['uiMap'][uiUUID] = fs[f].default;
-    }
-  }
-});
-
-import { ViewRender } from './util/view_render.jsx';
-/**
- * 渲染UI
- *
- * @returns DOM
- */
-// eslint-disable-next-line react-refresh/only-export-components
-function UIRender() {
-  var params = useParams() || {};
-
-  //把目前URL里的query参数存储到内置变量中
-  window.__Mokelay.InternalVar.URL_Search_Params = new URLSearchParams(useLocation().search);
-
-  //存储所有组件的Key和Ref
-  const ComponentInstantMap = {};
-  window.__Mokelay.ComponentInstantMap = ComponentInstantMap;
-
-  //获取Params参数
-  var appUUID = params['app_uuid'];
-  var uiUUID = params['ui_uuid'];
-
-  var appInfo = appMap[appUUID];
-
-  if (appInfo) {
-    var app = appInfo['app'];
-    var uiMap = appInfo['uiMap'];
-
-    //APP默认首页
-    var pageDefault = app['pages']['Page_Default'];
-    //APP的404页面
-    var page404 = app['pages']['Page_404'];
-
-    var renderUI = null;
-    if (typeof uiUUID == 'undefined') {
-      //默认显示APP首页
-      renderUI = uiMap[pageDefault];
-    } else {
-      //获取目标UI
-      renderUI = uiMap[uiUUID];
-      if (!renderUI) {
-        // 如果找不到配置，则返回该APP配置的404页面
-        renderUI = uiMap[page404];
-      }
-    }
-    document.title = Util.executeStr(renderUI['title']);
-    window.__Mokelay.UIViewConfig = renderUI['view'];
-    return <ViewRender view={renderUI['view']} />;
-  } else {
-    //TODO 找不到对应的APP信息，如何配置页面？
-    return <div>Can not found any app</div>;
-  }
-}
+import { UIRender } from './util/ui_render.jsx';
 
 /**
  * 开始正式渲染页面，挂载到root节点
---
+++ src/util/ui_render.jsx
@@ -0,0 +1,82 @@
+import { useParams, useLocation } from 'react-router-dom';
+import { ViewRender } from './view_render.jsx';
+import Util from './util.jsx';
+
+/**
+ * 通过URL获取页面参数，获取页面DSL，目前可以先从dsl/ui/目录里本地加载，方便测试
+ */
+//App信息
+import appList from '../../dsl/ui/app_list.js';
+//导入UI配置信息，开发环境为JS Config， 生产环境为接口获取
+const appMap = {};
+appList.map(function (_app) {
+  appMap[_app['uuid']] = {
+    app: _app,
+    uiMap: {},
+  };
+});
+const fs = import.meta.globEager('../../dsl/ui/*/*.js');
+Object.keys(fs).forEach(function (f) {
+  var _tmp = f.split('/');
+  if (_tmp.length == 6) {
+    const appUUID = _tmp[4];
+    const uiUUID = f.substr(f.lastIndexOf('/') + 1, f.lastIndexOf('.') - f.lastIndexOf('/') - 1);
+    if (appMap[appUUID]) {
+      appMap[appUUID]['uiMap'][uiUUID] = fs[f].default;
+    }
+  }
+});
+// console.log(appMap);
+
+/**
+ * 渲染UI
+ *
+ * @returns DOM
+ */
+export function UIRender() {
+  var params = useParams() || {};
+
+  //把目前URL里的query参数存储到内置变量中
+  window.__Mokelay.InternalVar.URL_Search_Params = new URLSearchParams(useLocation().search);
+
+  //存储所有组件的Key和Ref
+  const ComponentInstantMap = {};
+  window.__Mokelay.ComponentInstantMap = ComponentInstantMap;
+
+  //获取Params参数
+  var appUUID = params['app_uuid'];
+  var uiUUID = params['ui_uuid'];
+
+  var appInfo = appMap[appUUID];
+
+  if (appInfo) {
+    var app = appInfo['app'];
+    var uiMap = appInfo['uiMap'];
+
+    //APP默认首页
+    var pageDefault = app['pages']['Page_Default'];
+    //APP的404页面
+    var page404 = app['pages']['Page_404'];
+
+    var renderUI = null;
+    if (typeof uiUUID == 'undefined') {
+      //默认显示APP首页
+      renderUI = uiMap[pageDefault];
+    } else {
+      //获取目标UI
+      renderUI = uiMap[uiUUID];
+      if (!renderUI) {
+        // 如果找不到配置，则返回该APP配置的404页面
+        renderUI = uiMap[page404];
+      }
+    }
+    document.title = Util.executeStr(renderUI['title']);
+
+    //TODO 是否要放在window下？
+    window.__Mokelay.UIViewConfig = renderUI['view'];
+    return <ViewRender view={renderUI['view']} />;
+  } else {
+    //TODO 找不到对应的APP信息，如何配置页面？
+    return <div>Can not found any app</div>;
+  }
+}
