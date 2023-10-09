5eb2af6f71314871370f13abf5577b7fd3cb7554
CarlChen <carl@greatbee.com>
2023-10-09 19:07:26 +0800

    Text组件支持edit，为了给Tree的Node可编辑使用

--- src/component/m_text/index.jsx
+++ src/component/m_text/index.jsx
@@ -7,8 +7,12 @@
 import { forwardRef, useImperativeHandle } from 'react';
 import { useState } from 'react';
 
-const M_Text = forwardRef(function M_Text({ initContent, maxLine = 1 }, ref) {
+const M_Text = forwardRef(function M_Text(
+  { initContent, maxLine = 1, supportEdit = false, onContentChange },
+  ref,
+) {
   const [content, setContent] = useState(initContent);
+  const [editing, setEditing] = useState(false);
 
   useImperativeHandle(
     ref,
@@ -23,7 +27,26 @@ const M_Text = forwardRef(function M_Text({ initContent, maxLine = 1 }, ref) {
     [],
   );
 
-  return <span ref={ref}>{content}</span>;
+  var dbClick = function () {
+    if (supportEdit) {
+      setEditing(true);
+    }
+  };
+
+  var contentChange = function (e) {
+    //TODO
+    //监控keyCode = "Enter"
+    if (onContentChange) {
+      onContentChange({ e: e, content: content });
+    }
+  };
+
+  return (
+    <span ref={ref} onDoubleClick={dbClick}>
+      {!editing && content}
+      {editing && <input value={content} onChange={contentChange} />}
+    </span>
+  );
 });
 
 M_Text.displayName = 'M_Text';
